dce1908ec8abe9d787fd92467ce3c940739ae942
vega_moritake <moritake.nobuo@vega-c.com>
2019-12-02 15:03:45 +0900

    Add skelanimation

--- convert_armature.py
+++ convert_armature.py
@@ -2,51 +2,127 @@ import bpy
 import numpy as np
 import math
 import os
+import copy
 import shutil
+from mathutils import Matrix
 
-from . import target
-from .target import Rename
+from . import utils
+from .utils import Rename
 
 
 
-def ConvertSkeleton():
-    usda = ""
-    return usda
+def SkelAnimationData(bones):
+    translations = []
+    rotations = []# quaternion
+    scales = []
 
-def ConvertSkelAnimation():
-    usda = ""
+    scn = bpy.context.scene
+    orig_frame = scn.frame_current
+
+    for frame in range(scn.frame_start, scn.frame_end+1):
+        scn.frame_set(frame)
+
+        translations_old = []
+        rotations_old = []
+        scales_old = []
+        for bone in bones:
+            loc, rot, scale = bone.matrix.decompose()
+            translations_old.append(loc[:])
+            rotations_old.append(rot[:])
+            scales_old.append(scale[:])
+
+        if not translations or translations[-1][1] != translations_old or frame == scn.frame_end:
+            translations += [(frame, copy.copy(translations_old))]
+            
+        if not rotations or rotations[-1][1] != rotations_old or frame == scn.frame_end:
+            rotations += [(frame, copy.copy(rotations_old))]
+
+        if not scales or scales[-1][1] != scales_old or frame == scn.frame_end:
+            scales += [(frame, copy.copy(scales_old))]
+        
+    scn.frame_set(orig_frame)
+
+    return translations, rotations, scales
+
+
+
+def ConvertSkel(obj_armature):
+    armature = obj_armature.data
+
+    usda = """
+    def """+'"'+Rename(armature.name)+'"'+"""
+    {
+        rel skel:skeleton = </Armatures/"""+Rename(armature.name)+"""/skeleton>
+        rel skel:animationSource = </Armatures/"""+Rename(armature.name)+"""/animation>"""
+        
+    # animation data
+    usda += """
+
+        def SkelAnimation "animation"
+        {"""
+
+    if utils.keywords["include_animation"]:
+        translations, rotations, scales = SkelAnimationData(obj_armature.pose.bones)
+
+        joints = []
+        for bone in armature.bones:
+            bone_name = bone.name
+            bo = bone
+            while bo.parent:
+                bo = bo.parent
+                bone_name = bo.name +'/'+ bone_name
+            joints.append(Rename(bone_name))
+
+        usda += """
+            uniform token[] joints = """+str(joints).replace("'", '"')
+            
+        usda += """
+            float3[] translations.timeSamples = {"""
+        for frame, translation in translations:
+            usda += """
+                """+str(frame)+": "+str(translation)+","
+        usda += """
+            }
+            quatf[] rotations.timeSamples = {"""
+        for frame, rotation in rotations:
+            usda += """
+                """+str(frame)+": "+str(rotation)+","
+        usda += """
+            }
+            half3[] scales.timeSamples = {"""
+        for frame, scale in scales:
+            usda += """
+                """+str(frame)+": "+str(scale)+","
+        usda += """
+            }"""
+
+    usda += """
+        }
+    }"""
+    
     return usda
 
 
 
 def ConvertArmatures():
-    if not target.keywords["include_armatures"]:
+    if not utils.keywords["include_armatures"]:
         return ""
+    
+    # get armatures
+    armatures = []
+    for obj in utils.objects:
+        for mod in obj.modifiers:
+            if mod.bl_rna.identifier == 'ArmatureModifier' and mod.object:
+                armatures.append(mod.object)
+    armatures = list(set(armatures))
 
     usda = """
 
 def Scope "Armatures"
 {"""
 
-    # get armatures
-    armatures = []
-    for obj in target.objects:
-        for mod in obj.modifiers:
-            if mod.bl_rna.identifier == 'ArmatureModifier' and mod.object:
-                armatures.append(mod.object.data)
-    armatures = list(set(armatures))
-
     for armature in armatures:
-        usda += """
-    def """+'"'+Rename(armature.name)+'"'+"""
-    {
-        rel skel:animationSource = </Armatures/"""+Rename(armature.name)+"""/animation>
-        rel skel:skeleton = </Armatures/"""+Rename(armature.name)+"""/skeleton>"""
-        
-        usda += ConvertSkeleton()
-        usda += ConvertSkelAnimation()
-        usda += """
-    }"""
+        usda += ConvertSkel(armature)
     
     usda += """
 }"""
