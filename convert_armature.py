3c6809cf99ff8f37a55afb769a7ef69acec31ff1
vega_moritake <moritake.nobuo@vega-c.com>
2019-12-09 18:26:42 +0900

    Fix rest and bind transform

--- convert_armature.py
+++ convert_armature.py
@@ -6,8 +6,54 @@ import copy
 import shutil
 from mathutils import Matrix
 
+from bpy_extras.io_utils import axis_conversion
 from . import utils
 from .utils import Rename
+
+
+
+
+def ConvertSkeleton(usda, obj_armature):
+    armature = obj_armature.data
+    joints = []
+    restTransforms = []
+    bindTransforms = []
+    armature.pose_position = 'REST'
+
+    for bone in armature.bones:
+        bone_name = bone.name
+        bo = bone
+        while bo.parent:
+            bo = bo.parent
+            bone_name = bo.name +'/'+ bone_name
+        joints.append(Rename(bone_name))
+
+        global_matrix = (axis_conversion(to_forward='-Z', to_up='Y').to_4x4())
+    
+        # Specifies the rest-pose transforms of each joint in local space
+        mat = global_matrix @ obj_armature.pose.bones[bone.name].matrix
+        for i in range(3):
+            mat[i][3] *= 0.01
+        matrix = [tuple(v) for v in mat]
+        restTransforms.append(tuple(matrix)) 
+
+        # Specifies the bind-pose transforms of each joint in world space
+        mat = obj_armature.matrix_world @ global_matrix @ armature.bones[bone.name].matrix_local
+        for i in range(3):
+            mat[i][3] *= 0.01
+        matrix = [tuple(v) for v in mat]
+        bindTransforms.append(tuple(matrix))
+    
+    armature.pose_position = 'POSE'
+
+    usda.append(f"""
+        def Skeleton "skeleton"
+        {{
+            uniform token[] joints = {str(joints).replace("'", '"')}
+            uniform matrix4d[] restTransforms = {restTransforms}
+            uniform matrix4d[] bindTransforms = {bindTransforms}
+        }}"""
+    )
 
 
 
@@ -26,7 +72,7 @@ def SkelAnimationData(bones):
         rotations_old = []
         scales_old = []
         for bone in bones:
-            if bone.parent and bone.bone.use_connect:
+            if bone.parent:
                 par_inv = bone.parent.matrix_channel.inverted_safe()
             else:
                 par_inv = Matrix()
@@ -56,14 +102,10 @@ def SkelAnimationData(bones):
 
 
 
-def ConvertSkel(usda, obj_armature):
+def ConvertSkelAnim(usda, obj_armature):
     armature = obj_armature.data
 
     usda.append(f"""
-    def "{Rename(armature.name)}"
-    {{
-        rel skel:skeleton = </Armatures/{Rename(armature.name)}/skeleton>
-        rel skel:animationSource = </Armatures/{Rename(armature.name)}/animation>
 
         def SkelAnimation "animation"
         {{"""
@@ -109,8 +151,7 @@ def ConvertSkel(usda, obj_armature):
             }"""
         )
     usda.append("""
-        }
-    }"""
+        }"""
     )
 
 
@@ -133,8 +174,19 @@ def ConvertArmatures(usda):
 def Scope "Armatures"
 {""")
 
-    for armature in armatures:
-        ConvertSkel(usda, armature)
+    for armature in utils.armatures:
+        usda.append(f"""
+    def "{Rename(armature.name)}"
+    {{
+        rel skel:skeleton = </Armatures/{Rename(armature.name)}/skeleton>
+        rel skel:animationSource = </Armatures/{Rename(armature.name)}/animation>"""
+        )
+        ConvertSkeleton(usda, armature)
+        ConvertSkelAnim(usda, armature)
+
+        usda.append("""
+    }"""
+        )
     
     usda.append("""
 }""")
